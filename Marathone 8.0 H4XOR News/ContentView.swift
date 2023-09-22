36b7d775cdad08548b5baccfd36c64c544c650a1
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-22 18:36:34 +0500

    Initial Commit

---
+++ Marathone 8.0 H4XOR News/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Marathone 8.0 H4XOR News
+//
+//  Created by Ислам Пулатов on 9/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marathone 8.0 H4XOR News/Marathone_8_0_H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marathone_8_0_H4XOR_NewsApp.swift
+//  Marathone 8.0 H4XOR News
+//
+//  Created by Ислам Пулатов on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Marathone_8_0_H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
